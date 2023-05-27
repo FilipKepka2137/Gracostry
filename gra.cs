31a34457873e3add6fd9ec5611b4db0a9e7f065b
FilipKepka2137 <filip.kepka@gmail.com>
2023-05-27 12:55:49 +0200

    Create gra.cs

---
+++ gra.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gra_wyrzutnia_rakiet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerName;
+            int playerHealth = 100;
+            int playerDamage = 20;
+            int enemyHealth = 50;
+            int enemyDamage = 10;
+
+            Console.WriteLine("Witaj w gułagu!");
+            Console.Write("Jak ci na imie?: ");
+            playerName = Console.ReadLine();
+
+            Console.WriteLine("Siema, " + playerName + " Zaczynajmy!.");
+
+            while (playerHealth > 0 && enemyHealth > 0)
+            {
+                Console.WriteLine("\n" + playerName + "'s HP: " + playerHealth);
+                Console.WriteLine("HP Przeciwnika: " + enemyHealth);
+                Console.WriteLine("1. Atak");
+                Console.WriteLine("2. Obrona");
+                Console.WriteLine("3. Ucieczka");
+
+                Console.Write("Wybieraj co robisz (1-3): ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine(playerName + " Atak");
+                        enemyHealth -= playerDamage;
+                        Console.WriteLine("Wróg traci " + playerDamage + " HP.");
+                        break;
+                    case "2":
+                        Console.WriteLine(playerName + " broni się przed atakiem wroga.");
+                        playerHealth -= enemyDamage / 2;
+                        Console.WriteLine("Przegrałes " + (enemyDamage / 2) + " HP.");
+                        break;
+                    case "3":
+                        Console.WriteLine(playerName + " próbujesz uciec.");
+                        if (new Random().Next(1, 3) == 1)
+                        {
+                            Console.WriteLine("Udało ci się uciec!");
+                            enemyHealth = 0;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nie udało ci się uciec!");
+                            playerHealth -= enemyDamage;
+                            Console.WriteLine("Wróg cię zaatakował i tracisz " + enemyDamage + " HP.");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Mordo nie umiesz czytać czy pisać?.");
+                        break;
+                }
+
+                if (enemyHealth > 0)
+                {
+                    Console.WriteLine("\nPrzeciwnik cię atakuje!");
+                    playerHealth -= enemyDamage;
+                    Console.WriteLine("Tracisz " + enemyDamage + " HP.");
+                }
+
+                if (playerHealth <= 0)
+                {
+                    Console.WriteLine("\n" + playerName + " Game over Christopher!");
+                }
+                else if (enemyHealth <= 0)
+                {
+                    Console.WriteLine("\nWygrywasz!!!");
+                }
+            }
+
+            Console.WriteLine("\nKoniec!");
+            Console.ReadLine();
+        }
+    }
+}
